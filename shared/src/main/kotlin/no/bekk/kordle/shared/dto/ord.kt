f42727499ce475f4720834da1dd171b5442c56a4
Patrick Allport <patrick@bekk-mac-3124.shared>
2025-07-22 21:51:53 +0200

    La til serializable-tags på DTOer

--- shared/src/main/kotlin/no/bekk/kordle/shared/dto/ord.kt
+++ shared/src/main/kotlin/no/bekk/kordle/shared/dto/ord.kt
@@ -1,26 +1,33 @@
 package no.bekk.kordle.shared.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Oppgave(
     val id: Int,
     val ord: String,
     val lengde: Int,
 )
 
+@Serializable
 data class LeggTilOrdRequest(
     val ord: String
 )
 
+@Serializable
 data class GjettOrdRequest(
     val oppgaveId: Int,
     val ordGjett: String
 )
 
 
+@Serializable
 data class GjettResponse(
     val oppgaveId: Int,
     val alleBokstavtreff: List<BokstavTreff>
 )
 
+@Serializable
 data class BokstavTreff(
     val plassISekvensen: Int,
     val bokstavGjettet: String,
